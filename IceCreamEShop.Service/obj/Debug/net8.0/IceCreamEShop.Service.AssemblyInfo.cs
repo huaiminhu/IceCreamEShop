3d5dfc4fb2b35d952c482339b6c6ae39f0ba2c8b
Hank Hu <hankhuwm@gmail.com>
2026-05-17 14:50:48 +0800

    新增專案分層

--- IceCreamEShop.Service/obj/Debug/net8.0/IceCreamEShop.Service.AssemblyInfo.cs
+++ IceCreamEShop.Service/obj/Debug/net8.0/IceCreamEShop.Service.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("IceCreamEShop.Web")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("IceCreamEShop.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+497a1c54055007f6c95c0f1e9f8778ec689c3521")]
-[assembly: System.Reflection.AssemblyProductAttribute("IceCreamEShop.Web")]
-[assembly: System.Reflection.AssemblyTitleAttribute("IceCreamEShop.Web")]
+[assembly: System.Reflection.AssemblyProductAttribute("IceCreamEShop.Service")]
+[assembly: System.Reflection.AssemblyTitleAttribute("IceCreamEShop.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 類別產生。
